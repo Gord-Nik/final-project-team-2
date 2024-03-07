87079971adfa607136403c5646b1171cf8307bed
Gord-Nik <ua.gord.nik@gmail.com>
2024-03-07 14:07:53 +0200

    codestyle

--- models.py
+++ models.py
@@ -49,7 +49,8 @@ class Record:
         return [p.value for p in self.phones if p.value == phone]
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+        return (f"Contact name: {self.name.value}, "
+                f"phones: {'; '.join(str(p) for p in self.phones)}")
 
 
 class AddressBook(UserDict):
@@ -89,7 +90,8 @@ if __name__ == "__main__":
     john = book.find("John")
     john.edit_phone("1234567890", "1112223333")
 
-    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+    print(john)  # Виведення: Contact name: John,
+    # phones: 1112223333; 5555555555
 
     # Пошук конкретного телефону у записі John
     found_phone = john.find_phone("5555555555")
