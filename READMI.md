404f9947a8a61cc824b6d21cad3d70dde4f3fa60
Gord-Nik <ua.gord.nik@gmail.com>
2024-03-12 12:13:14 +0200

    add setup, running from cmd

--- READMI.md
+++ READMI.md
@@ -1,1 +1,7 @@
-final project
+final project team 2
+
+for setup bot assistant you need:
+
+1.pull this project to your laptop
+2.go to project directory from command line and run "pip install ." command
+3.run "bot_assistant" command by command line for work with bot
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='final-project',
+    version='1',
+    description='Final project team 2',
+    url='https://github.com/Gord-Nik/final-project-team-2',
+    author='Nikita Hordiienko, Nataliya Traxler, Roman Koreshniak, Yevhenii Stakhovskyi ',
+    author_email='ua.gord.nik@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages(),
+    install_requires=['docutils'],
+    entry_points={'console_scripts': ['bot_assistant = src.cli_bot:main']}
+)
